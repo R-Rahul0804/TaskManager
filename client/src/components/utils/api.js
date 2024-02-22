abdcd2ee9c2affa8d6e1b373a0ef2ee58240a4f0
R-Rahul0804 <rahul08042001@gmail.com>
2024-02-22 12:46:09 +0530

    11th commit

--- client/src/components/utils/api.js
+++ client/src/components/utils/api.js
@@ -45,13 +45,6 @@ api.interceptors.request.use(
   }
 );
 
-// Add a response interceptor
-// axios.interceptors.response.use(function (response) {
-//   // Do something with response data
-//   return response;
-// }, function (error) {
-//   // Do something with response error
-//   return Promise.reject(error);
-// });
+
 
 export default api;
